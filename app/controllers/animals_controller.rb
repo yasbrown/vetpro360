125f0122535b2bb60fdad6320f3909bcdd1c62ee
yasmin brown <yasminbrown6@hotmail.com>
2023-03-21 14:40:17 +0000

    add new animal

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,10 +1,24 @@
 class AnimalsController < ApplicationController
+  before_action :set_user, only: [:new, :create]
+
   def new
-    @user = User.find(params[:user_id])
     @animal = Animal.new
   end
 
   def create
+    @animal = Animal.new(animal_params)
+    @animal.user = @user
+    @animal.save
+    redirect_to user_path(@user)
+  end
 
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def animal_params
+    params.require(:animal).permit(:name, :species, :gender)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
-    @animal = Animal.find_by(user: @user.id)
+    @animals = Animal.where(user: @user.id)
   end
 end
