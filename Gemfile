ff1e5d810e9a77edd215fb0dbf89de7eaf31d4f7
yasmin brown <yasminbrown6@hotmail.com>
2023-03-22 15:34:57 +0000

    devise set up

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ ruby "3.1.2"
 
 gem 'simple_form'
 gem 'sass-rails'
-
+gem 'devise'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4", ">= 7.0.4.2"
--- db/migrate/20230320152155_create_users.rb
+++ db/migrate/20230320152155_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :name
       t.text :address
-      t.string :email
       t.string :phone_number
       t.boolean :owner, default: false
 
