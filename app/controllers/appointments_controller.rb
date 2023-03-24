f52c1d609f6a407718f34018656d918e6da675a4
yasmin brown <yasminbrown6@hotmail.com>
2023-03-24 18:44:39 +0000

    creating an appointment logic

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,8 +1,7 @@
 class AppointmentsController < ApplicationController
   def index
-    @all_slots = Slot.all
     @available_slots = Slot.where(available: true)
-    @booked_appointments = Appointment.all
+    @booked_appointments = Appointment.all.sort_by { |element| element.start_time }
   end
 
   def show
@@ -16,14 +15,29 @@ class AppointmentsController < ApplicationController
 
   def create
     @appointment = Appointment.new(appointment_params)
+
     @slot = Slot.find(params[:slot_id])
     @appointment.slot = @slot
-    @vet = Vet.find(params[:vet_id])
+
+    @vet = Vet.find(params[:appointment][:vet_id])
     @appointment.vet = @vet
-    @animal = Animal.find(params[:animal_id])
+
+    @animal = Animal.find(params[:appointment][:animal_id])
     @appointment.animal = @animal
+
+    @appointment.start_time = @slot.start_time
+    @appointment.end_time = @slot.end_time
+
     @slot.available = false
+    @slot.save!
+
     @appointment.save!
     redirect_to root_path
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:reason_for_appointment, :animal_id, :vet_id, :slot_id)
   end
 end
