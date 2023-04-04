4da02e94e3ee811b1ea12d2e5e21252fe4b6c301
yasmin brown <yasminbrown6@hotmail.com>
2023-04-04 14:53:22 +0100

    added validations to appointment so appointment cannot be booked in the past

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,4 +5,13 @@ class Appointment < ApplicationRecord
 
   validates :start_time, presence: true
   validates :end_time, presence: true
+  validates :no_past_appointment
+
+  def no_past_appointment
+    if Date.today > start_time.to_date || Time.now > start_time.to_time
+      errors.add(:start_time, "cannot be in the past")
+    elsif Date.today > end_time.to_date || Time.now > end_time.to_time
+      errors.add(:end_time, "cannot be in the past")
+    end
+  end
 end
