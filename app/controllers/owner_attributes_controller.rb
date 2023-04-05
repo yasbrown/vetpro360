3ac4642312c210fa26fba7ce7cec0b98b067e886
yasmin brown <yasminbrown6@hotmail.com>
2023-04-05 15:44:45 +0100

    edit/update owner

--- app/controllers/owner_attributes_controller.rb
+++ app/controllers/owner_attributes_controller.rb
@@ -1,4 +1,6 @@
 class OwnerAttributesController < ApplicationController
+  before_action :set_owner, only: [:edit, :update]
+
   def index
     @owners = OwnerAttribute.all
   end
@@ -8,5 +10,35 @@ class OwnerAttributesController < ApplicationController
 
     @animals = Animal.where(owner_attribute: @owner.id, archived: false)
     @archived_animals = Animal.where(owner_attribute: @owner.id, archived: true)
+  end
+
+  def new
+    @user = User.new
+    @owner = OwnerAttribute.new
+  end
+
+  def create
+    @owner = OwnerAttribute.new(owner_params)
+    @owner.user = @user
+    @owner.save!
+    redirect_to owner_attribute_path(@owner)
+  end
+
+  def edit
+  end
+
+  def update
+    @owner.update(owner_params)
+    redirect_to  owner_attribute_path(@owner)
+  end
+
+  private
+
+  def owner_params
+    params.require(:owner_attribute).permit(:first_name, :last_name, :address, :phone_number)
+  end
+
+  def set_owner
+    @owner = OwnerAttribute.find(params[:id])
   end
 end
