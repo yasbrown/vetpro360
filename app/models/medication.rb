fbbda01a4f721438db5e5b5b8beaaafb8b992338
yasmin brown <yasminbrown6@hotmail.com>
2023-04-25 21:58:00 +0100

    parse medicine API and store medication names in csv file

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,9 +1,34 @@
-class Medication < ApplicationRecord
-  belongs_to :animal
-  belongs_to :vet_attribute
-  validates :quantity_dispensed, presence: true,
-                                  numericality: { only_integer: true }
-  validates :start_date, presence: true
-  validates :end_date, presence: true
-  validates :quantity_dispensed, numericality: { only_integer: true }
-end
+require "json"
+require "open-uri"
+require "csv"
+
+# class Medication < ApplicationRecord
+  # belongs_to :animal
+  # belongs_to :vet_attribute
+  # validates :quantity_dispensed, presence: true,
+  #                                 numericality: { only_integer: true }
+  # validates :start_date, presence: true
+  # validates :end_date, presence: true
+  # validates :quantity_dispensed, numericality: { only_integer: true }
+
+  medication_list = []
+  csv_filepath = "db/medication_list.csv"
+
+  for num in (1..197)
+    medication_url = "https://dailymed.nlm.nih.gov/dailymed/services/v2/drugnames.json?page=#{num}&pagesize=100"
+    medication_hash = JSON.parse(URI.open(medication_url).read)
+    medication_array = medication_hash['data']
+
+    medication_array.map do |medication|
+      medication_list << medication['drug_name']
+    end
+  end
+
+  CSV.open(csv_filepath, "wb") do |csv|
+    csv << ["Medication Name"]
+
+    medication_list.each do |meds|
+      csv << [meds]
+    end
+  end
+# end
